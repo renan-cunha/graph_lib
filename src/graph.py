dd34c5c9e8b68a809b4cc9c2951f2a397d2329e6
renancunha <renancunhafonseca@hotmail.com>
2019-04-10 10:28:14 -0300

    max degree function with generators

--- src/graph.py
+++ src/graph.py
@@ -19,10 +19,10 @@ class Graph:
         The vertice 4 has a loop
         """
         self.__assert_positive_vertice(number_of_vertices)
-        self.graph: Dict[int: List[int]] = {}
+        self.__graph: Dict[int: List[int]] = {}
 
         for i in range(number_of_vertices):
-            self.graph[i] = []
+            self.__graph[i] = []
 
         self.__number_of_vertices = number_of_vertices
         self.__number_of_edges = 0
@@ -37,25 +37,22 @@ class Graph:
 
     def add_edge(self, v: int, w: int) -> None:
         """Adds an edge between vertices v and w"""
-        self.__verify_vertices_exists([v, w])
-        self.graph[v].append(w)
+        self.__graph[v].append(w)
 
         # only adds two edges if not a loop
         if v != w:
-            self.graph[w].append(v)
+            self.__graph[w].append(v)
 
         self.__number_of_edges += 1
 
     def neighbourhood(self, v: int) -> List:
         """Returns a list with the vertices that have an edge with the vertice
         v"""
-        self.__verify_vertices_exists([v])
-        return self.graph[v]
+        return self.__graph[v]
 
     def degree(self, v: int) -> int:
-        """Returns the number of edges that a vertice has.
+        """Returns the degree of a vertice, sum of all edges.
         Remember: Loops count as two edges"""
-        self.__verify_vertices_exists([v])
         count = 0
         for connected_vertice in self.neighbourhood(v):
             count += 2 if connected_vertice == v else 1
@@ -64,20 +61,12 @@ class Graph:
     def max_degree(self) -> Tuple[int, int]:
         """Returns a tuple with the vertice with maximum degree and it's
         degree"""
-
-        max_vertice, max_degree = 0, 0
-
-        for vertice in self.graph:
-            vertice_degree = self.degree(vertice)
-
-            if vertice_degree > max_degree:
-                max_vertice, max_degree = vertice, vertice_degree
-
-        return max_vertice, max_degree
+        degrees = [self.degree(vertice) for vertice in self.__graph]
+        maximum_degree = max(degrees)
+        return degrees.index(maximum_degree), maximum_degree
 
     def number_of_loops_vertice(self, vertice: int) -> int:
         """Returns the number of loops on a vertice"""
-        self.__verify_vertices_exists([vertice])
 
         count = 0
         for connected_vertice in self.neighbourhood(vertice):
@@ -90,22 +79,16 @@ class Graph:
         """Returns the number of loops on the graph"""
 
         count = 0
-        for vertice in self.graph:
+        for vertice in self.__graph:
             count += self.number_of_loops_vertice(vertice)
+
         return count
 
     def __str__(self) -> str:
         result = "\nVertices: Connected Vertices\n"
-        for vertice in self.graph:
-            result += f"{vertice}: {self.graph[vertice]}\n"
+        for vertice in self.__graph:
+            result += f"{vertice}: {self.__graph[vertice]}\n"
         return result
-
-    def __verify_vertices_exists(self, vertices: list) -> None:
-        """Raise an exception if vertice is not encountered"""
-        for vertice in vertices:
-            if vertice not in self.graph:
-                raise ValueError(f"Vertice {vertice} does not exist on this"
-                                 f" graph")
 
     @staticmethod
     def __assert_positive_vertice(number: int) -> None:
