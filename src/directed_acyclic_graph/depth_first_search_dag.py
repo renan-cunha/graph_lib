b0b9cb8f674b8b4f2f2b25a6863f7b6f6e8e4ee0
Renan Cunha <renancunhafonseca@gmail.com>
2019-05-28 12:17:57 -0300

    DFS to detect DAG, used befor topological sorting starts

---
+++ src/directed_acyclic_graph/depth_first_search_dag.py
@@ -0,0 +1,42 @@
+from src.graph_matrix import GraphMatrix
+from typing import List
+from src.search.color import Color
+
+
+class DepthFirstSearchDAG:
+    """Class that performs the depth-first search algorithm"""
+    def __init__(self, graph: GraphMatrix, list_of_vertices: List[int] = None):
+        if not graph.digraph:
+            raise ValueError("this graph is not directed")
+        self.color: List[Color] = [Color.WHITE] * graph.number_of_vertices()
+        self.discovered_vertice_time: List = [None]*graph.number_of_vertices()
+        self.final_vertice_time: List = [None]*graph.number_of_vertices()
+        self.predecessor: List = [None]*graph.number_of_vertices()
+        self.count: int = 0
+        self.dag = True
+
+        if not list_of_vertices:
+            list_of_vertices = graph.get_vertices()
+        for vertice in list_of_vertices:
+            if self.color[vertice] != Color.BLACK:
+                self.run(graph, vertice)
+
+    def run(self, graph: GraphMatrix, vertice: int) -> None:
+        self.color[vertice] = Color.GRAY
+        self.count += 1
+        self.discovered_vertice_time[vertice] = self.count
+        for connected_vertice in graph.out_neighbourhood(vertice):
+            if self.color[connected_vertice] == Color.WHITE:
+                self.predecessor[connected_vertice] = vertice
+                self.run(graph, connected_vertice)
+
+            # if DFS finds a gray vertice, than it is not DAG
+            elif self.color[connected_vertice] == Color.GRAY:
+                self.dag = False
+        self.color[vertice] = Color.BLACK
+        self.count += 1
+        self.final_vertice_time[vertice] = self.count
+
+    def is_dag(self) -> bool:
+        return self.dag
+
--- src/directed_acyclic_graph/topological_sorting/topological_sorting.py
+++ src/directed_acyclic_graph/topological_sorting/topological_sorting.py
@@ -1,17 +1,22 @@
 from src.graph_matrix import GraphMatrix
 from typing import List
-from src.search.depth_first_search_digraph import DepthFirstSearch
+from src.directed_acyclic_graph.depth_first_search_dag import DepthFirstSearchDAG
 
 
 def topological_sorting(graph: GraphMatrix) -> List[int]:
-    dfs = DepthFirstSearch(graph)
+    if not graph.digraph:
+        raise ValueError("Not a digraph")
+
+    dfs = DepthFirstSearchDAG(graph)
+
+    if not dfs.is_dag():
+        raise ValueError("Not a DAG")
+
     times = dfs.final_vertice_time
-    print(times)
     len_times = len(times)
     times = [[i, times[i]] for i in range(len_times)]
     times.sort(key=lambda x: x[1], reverse=True)
     return [times[i][0] for i in range(len_times)]
-
 
 
 
